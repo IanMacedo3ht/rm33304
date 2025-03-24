a5234ad2dbc687d93480c6454309baabd0d2fb96
Ian Mota Macedo <ian.macedo@etec.sp.gov.br>
2025-03-23 21:14:41 -0300

    adicionando os comandos do meu site

---
+++ script.js
@@ -0,0 +1,12 @@
+const botao = document.getElementById('botao');
+const imagem = document.getElementById('minhaImagem');
+
+
+botao.addEventListener('click', function() {
+  imagem.src = 'C:\Users\imota\OneDrive\Documentos\sites\gato.png';
+  
+}
+)
+
+imagem.style.display = 'block';
+;
