8a57b7c78352b35d5d2b2330e6918fdc201c86ed
Ian Mota Macedo <ian.macedo@etec.sp.gov.br>
2025-03-31 12:23:12 -0300

    criando o jogo

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-const botao = document.getElementById('botao');
-const imagem = document.getElementById('minhaImagem');
+const jogoContainer = document.querySelector(".container");
+const playerResult = document.querySelector(".player_result img");
+const botResult = document.querySelector(".bot_result img");
+const resultado = document.querySelector(".resultado");
+const opcoes = document.querySelectorAll(".opcao");
 
+const botImagens = ["images/pedra.png", "images/papel.png", "images/tesoura.png"];
+const outcomes = {
+  RR: "Empate",
+  RP: "BOT Ganhou",
+  RS: "VOCÊ Ganhou",
+  PP: "Empate",
+  PR: "VOCÊ Ganhou",  
+  PS: "BOT Ganhou",
+  SS: "Empate", 
+  SR: "BOT Ganhou",
+  SP: "VOCÊ Ganhou",
+};
 
-botao.addEventListener('click', function() {
-  imagem.src = 'C:\Users\imota\OneDrive\Documentos\sites\gato.png';
-  
+function handleOptionClick(event) {
+  const Imagemclicada = event.currentTarget;
+  const clique = Array.from(opcoes).indexOf(Imagemclicada);
+
+  playerResult.src = botResult.src = "images/pedra.png";
+  resultado.textContent = "Espere...";
+
+  opcoes.forEach((image, index) => {
+    image.classList.toggle("active", index === clique);
+  });
+
+  jogoContainer.classList.add("start");
+
+  setTimeout(() => {
+    jogoContainer.classList.remove("start");
+
+    const playerImagemSrc = Imagemclicada.querySelector("img").src;
+    playerResult.src = playerImagemSrc;
+
+    const Numeroale = Math.floor(Math.random() * botImagens.length);
+    const botImagemSrc = botImagens[Numeroale];
+    botResult.src = botImagemSrc;
+
+    const playerValue = ["R", "P", "S"][clique];
+    const botValue = ["R", "P", "S"][Numeroale];
+    const tecla = playerValue + botValue;
+    const outcome = outcomes[tecla] || "Unknown";
+
+    resultado.textContent = playerValue === botValue ? "Partida empatada" : `${outcome} `;
+  }, 2500);
 }
-)
 
-imagem.style.display = 'block';
-;
+opcoes.forEach(image => {
+  image.addEventListener("click", handleOptionClick);
+});
